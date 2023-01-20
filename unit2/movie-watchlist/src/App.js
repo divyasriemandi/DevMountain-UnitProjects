1788514f453ee5736960a2a48c464a6394f8cb8d
Divya Sri Emandi <117112551+divyasriemandi@users.noreply.github.com>
2023-01-20 09:58:33 -0600

    Project 2
    Project 2 file check in

---
+++ unit2/movie-watchlist/src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Header from "./components/Header";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import MovieScreen from "./components/MovieScreen";
+import Watchlist from "./components/Watchlist";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const addMovie = (movie) => setList([...list, movie]);
+
+  const removeMovie = (movie) => {
+    const newState = list.filter((mov) => {
+      return mov !== movie;
+    });
+    setList(newState);
+  };
+
+
+const getData = () => {
+    axios
+       // .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+       .get(`https://api.themoviedb.org/3/movie/popular?api_key=cdc0736c29fdba27c3f92526a3041b90&language=en-US&page=${page}`) 
+       .then((res) => {
+            console.log(res.data.results);
+            setMovieList(res.data.results);
+        });
+};
+
+useEffect(() => {
+    getData();
+}, [page]);
+
+return (
+  <div className="App">
+      <Header />
+      <main>
+      <MovieScreen
+            addMovie={addMovie}
+            movieList={movieList}
+            page={page}
+            setPage={setPage}
+            list={list}
+            removeMovie={removeMovie}
+          />
+          <Watchlist list={list} removeMovie={removeMovie}/>
+        </main>
+      </div>
+    );
+  }
+  
+  export default App;
+  

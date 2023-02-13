4aeac76e4bbb14b9d1cddb342a3742d243bec3c3
Divya Sri Emandi <117112551+divyasriemandi@users.noreply.github.com>
2023-02-12 19:37:52 -0600

    commit
    commit

---
+++ unit5/react-proj-5/starter/src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import  potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
+import displayCountryReducer from "../redux/slices/displayCountrySlice";
+
+
+
+export default configureStore({
+    reducer: { 
+        potentialCountries: potentialCountriesReducer,
+        displayCountry: displayCountryReducer,
+     },
+});
